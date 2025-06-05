1fe9437fdf85608e35d808ea970a2690bed83fa5
pawanacharya73 <pawanacharya832@gmail.com>
2025-06-05 17:59:30 +0545

    generation

--- main.c
+++ main.c
@@ -13,6 +13,8 @@
 #define COLS SCREEN_WIDTH / CELL_SIZE
 #define GRIDLINE_WIDTH 1
 
+#define GENERATION_SPEED 10 // once each 10 game loop iteration
+
 struct point {
     int row, col;
     int state; // 0 is dead, 1 is alive
@@ -21,11 +23,9 @@ struct point {
 
 struct point points[ROWS][COLS];
 
-void start_game(SDL_Surface* surface) {
-    // TODO  
-}   
 
-void init_points() {
+void init_points(){
+
     // store points
     for (int row = 0; row < ROWS; row++) {
         for(int col = 0; col < COLS; col++) {
@@ -37,8 +37,57 @@ void init_points() {
     }
 }
 
+
+// update points
+void update_points() {
+    int next_state[ROWS][COLS];
+
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            int total_alive_neighbors = 0;
+
+            // for neighbors
+            for (int x = -1; x <= 1; x++) {
+                for (int y = -1; y <= 1; y++) {
+                    if (x == 0 && y == 0) continue; // skip itself
+
+                    int neigh_r = row + x;
+                    int neigh_c = col + y;
+
+                    if (neigh_r >= 0 && neigh_r < ROWS && neigh_c >= 0 && neigh_c < COLS) {
+                        if (points[neigh_r][neigh_c].state == 1) {
+                            total_alive_neighbors++;
+                        }
+                    }
+                }
+            }
+
+            // rules
+            if (points[row][col].state == 1) { // if already alive
+                if (total_alive_neighbors  == 2 || total_alive_neighbors == 3)
+                    next_state[row][col] = 1;
+                else
+                    next_state[row][col] = 0;
+            } else {
+                if (total_alive_neighbors == 3)
+                    next_state[row][col] = 1; // rule of birth
+                else
+                    next_state[row][col] = 0; // stays dead
+            }
+        }
+    }
+
+    // Copy back to original grid
+    for (int row = 0; row < ROWS; row++) {
+        for (int col = 0; col < COLS; col++) {
+            points[row][col].state = next_state[row][col];
+        }
+    }
+}
+
+
 void reset_all_points() {
-    
+
     for (int row = 0; row < ROWS; row++) {
         for(int col = 0; col < COLS; col++) {
             points[row][col].state = 0;
@@ -114,7 +163,20 @@ void handleMouseClick(SDL_Surface *surface, SDL_MouseButtonEvent *button) {
 
 int main() {
     // initializing SDL
-    SDL_Init(SDL_INIT_VIDEO);
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL couldn't be initialized! SDL_Errow: %s\n", SDL_GetError());
+        return 1;
+    }
+
+    // printing controls
+    printf("\n          Controls\n");
+    printf("\n    Right Click : Populate Cell\n");
+    printf("    Left Click  : Depopulate Cell\n");
+    printf("    Space       : Reset\n");
+    printf("    Enter       : Start Game\n");
+    printf("    ESC         : Pause Game\n");
+    printf("    R           : Restart Game\n");
+
     SDL_Window *window = SDL_CreateWindow("Conway's Game of Life", SCREEN_WIDTH, SCREEN_HEIGHT, 0);
     SDL_Surface *surface = SDL_GetWindowSurface(window);
 
@@ -126,7 +188,13 @@ int main() {
     init_points();
 
     int running = 1;
+
+    int gameStarted = 0;
+    int gamePaused = 0;
+
     int isMouseDown;
+
+    int generation_counter = 0;
 
     while(running) {
 
@@ -149,9 +217,16 @@ int main() {
                     }
 
                     if(event.key.key == SDLK_RETURN) {
-                        draw_points(surface);
-                        start_game(surface);
-                    } break;
+                        gameStarted = 1;
+                    }
+
+                    if (event.key.key == SDLK_R) {
+                        gameStarted = 0;
+                    }
+
+                    if (event.key.key == SDLK_ESCAPE) {
+                        gamePaused = (gamePaused == 0)? 1 : 0; 
+                    }
 
                 default:
                     break;
@@ -163,19 +238,31 @@ int main() {
 
         }
 
-        // re-draw white bg on each update
-        SDL_FillSurfaceRect(surface, &white_screen, COLOR_WHITE);
-
-        // draw grid
-        draw_grid(surface);
+        if (!gamePaused) {
 
 
-        draw_points(surface);
+            // re-draw white bg on each update
+            SDL_FillSurfaceRect(surface, &white_screen, COLOR_WHITE);
 
-        SDL_UpdateWindowSurface(window);
+            // draw grid
+            draw_grid(surface);
+
+            if (gameStarted) {
+                if (generation_counter > GENERATION_SPEED){
+                    update_points();
+                    generation_counter = 0;
+                } else {
+                    generation_counter++;
+                }
+            }
+
+            draw_points(surface);
+
+            SDL_UpdateWindowSurface(window);
+        }
 
 
-        SDL_Delay(50);
+        SDL_Delay(20);
     }
 
     SDL_DestroyWindow(window);
